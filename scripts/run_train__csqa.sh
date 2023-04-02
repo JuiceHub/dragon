2cc14c6e016b15a87a8e6854380982a8252ee352
JuiceHub <183328242@qq.com>
2023-04-02 21:42:35 +0800

    v2

--- scripts/run_train__csqa.sh
+++ scripts/run_train__csqa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 export TOKENIZERS_PARALLELISM=true
 export OMP_NUM_THREADS=4
 dt=`date '+%Y%m%d_%H%M%S'`
@@ -50,7 +50,7 @@ random_ent_emb=false
 fp16=true
 upcast=true
 
-load_model_path=models/general_model.pt
+load_model_path=models/model.pt.v2
 
 echo "***** hyperparameters *****"
 echo "dataset: $dataset"
@@ -65,7 +65,7 @@ save_dir_pref='runs'
 mkdir -p $save_dir_pref
 mkdir -p logs
 
-run_name=dragon__${dataset}_ih_${inhouse}_load__elr${elr}_dlr${dlr}_W${warmup_steps}_b${bs}_ufz${unfreeze_epoch}_e${n_epochs}_sd${seed}__${dt}
+run_name=v2_dragon__${dataset}_ih_${inhouse}_load__elr${elr}_dlr${dlr}_W${warmup_steps}_b${bs}_ufz${unfreeze_epoch}_e${n_epochs}_sd${seed}__${dt}
 log=logs/train__${run_name}.log.txt
 
 ###### Training ######
@@ -73,7 +73,7 @@ python3 -u dragon.py \
     --dataset $dataset \
     --encoder $encoder -k $k --gnn_dim $gnndim -elr $elr -dlr $dlr -bs $bs --seed $seed -mbs ${mbs} --unfreeze_epoch ${unfreeze_epoch} --encoder_layer=${encoder_layer} -sl ${max_seq_len} --max_node_num ${max_node_num} \
     --n_epochs $n_epochs --max_epochs_before_stop ${max_epochs_before_stop} --fp16 $fp16 --upcast $upcast --use_wandb true \
-    --save_dir ${save_dir_pref}/${dataset}/${run_name} --save_model 0 \
+    --save_dir ${save_dir_pref}/${dataset}/${run_name} --save_model 2 \
     --run_name ${run_name} \
     --load_model_path $load_model_path \
     --residual_ie $residual_ie \
